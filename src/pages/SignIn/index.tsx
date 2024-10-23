5fcd76725f62d6fd51abd5cd5cbe9428d60f9094
Nathan Chacon <chacondorodo@gmail.com>
2024-10-22 22:50:13 -0300

    feat: start improving login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,17 @@
 import useSignIn from "./hooks/useSignIn"
+import './style.css'
+import googeLogo from '../../assets/googleLogo.png'
 const SignIn = () => {
     const {onLogin} = useSignIn()
 
     return (
-        <button onClick={onLogin}>Login with google</button>
+        <section className="signIn">
+            <button className="signIn__google-btn" onClick={onLogin}>
+                <img src={googeLogo}/>
+                Continuar com Google
+            </button>
+        </section>
+        
     )
 }
 

c5b490baea519b4ffc73a22c0084dfcaa44aa2eb
Nathan Chacon <chacondorodo@gmail.com>
2024-10-30 22:20:13 -0300

    feat: small adjustments

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './style.css';
-import logo from '../../assets/logo.svg';
-const Nav: React.FC = () => {
-  return (
-    <nav className="nav">
-      <div className="nav__content">
-        {/* Logo */}
-        <div className="logo">
-          <img src={logo} alt="Logo" />
-        </div>
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import './style.css'; // Import the CSS file for styling
+import logo from '../../assets/logo.svg'; // Update this with the actual path to your logo image
 
-        {/* Navigation Links */}
-        <ul className="nav__links">
-          <li className='nav__link'>
-            <Link to="/">Home</Link>
-          </li>
-          <li className='nav__link'>
-            <Link to="/players">Procurar</Link>
-          </li>
-          <li className='nav__link'>
-            <Link to="/login">Entrar</Link>
-          </li>
-        </ul>
+function Nav() {
+  const [isOpen, setIsOpen] = useState(false); // State to manage dropdown visibility
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen); // Toggle the dropdown menu
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar__brand">
+        <img src={logo} alt="Logo" className="navbar__logo" /> {/* Logo image */}
+        MyApp
+      </div>
+      <div className="navbar__burger" onClick={toggleMenu}>
+        {isOpen ? '✖' : '☰'} {/* Burger icon or close icon */}
+      </div>
+      <div className={`navbar__dropdown ${isOpen ? 'open' : ''}`}>
+        <Link to="/" className="navbar__item">HOME</Link>
+        <Link to="/jogadores" className="navbar__item">JOGADORES</Link>
+        <Link to="/duvidas" className="navbar__item">DUVIDAS</Link>
+        <Link to="/entrar" className="navbar__item">ENTRAR</Link>
       </div>
     </nav>
   );
-};
+}
 
 export default Nav;

1566bf7dafaae37906e5952d3a73b5fdbe1333fe
Nathan Chacon <chacondorodo@gmail.com>
2024-11-02 15:26:36 -0300

    feat: improve bundling images

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,15 @@
 import useSignIn from "./hooks/useSignIn"
 import './style.css'
 import googeLogo from '../../assets/googleLogo.png'
+import signInBg from '../../assets/signInBg.jpg';
+
 const SignIn = () => {
     const {onLogin} = useSignIn()
 
     return (
-        <section className="signIn">
+        <section className="signIn" style={{
+            backgroundImage: `url(${signInBg})`,
+          }}>
             <button className="signIn__google-btn" onClick={onLogin}>
                 <img src={googeLogo}/>
                 Continuar com Google

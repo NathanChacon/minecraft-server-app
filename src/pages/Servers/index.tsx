d54ef48825cb98e0a7ee396aad20b007e02ba776
Nathan Chacon <chacondorodo@gmail.com>
2024-11-24 12:10:33 -0300

    feat: add message when there is no server

--- src/pages/Servers/index.tsx
+++ src/pages/Servers/index.tsx
@@ -8,7 +8,7 @@ import isValidSubscription from "../../utils/subscription";
 import { getVisibleServers } from "../../api/services/server";
 import ServerCard from "../../components/ServerCard";
 import { Helmet } from "react-helmet";
-
+import diamond from '../../assets/mineDiamond.svg'
 const Servers = () => {
   const { user } = useUser();
   const [userSubscriptionData, setUserSubscriptionData] = useState<any>(null);
@@ -56,47 +56,48 @@ const Servers = () => {
       </Helmet>
       <section className="servers primary-bg">
         <section className="servers__about-container">
-        <div className="servers__about">
-          <div className="servers__about-left">
-            <header className="servers__about-header">
-              <h1 className="servers__about-title">
-                Encontre os Melhores Servidores de Minecraft
-              </h1>
-            </header>
-            <p className="servers__about-text">
-              Descubra uma seleção incrível de servidores para todos os estilos de jogo. Conecte-se com comunidades ativas,
-              explore mundos personalizados e participe de aventuras épicas.
-            </p>
-            <p className="servers__about-text">
-              Tem um servidor? Publique-o agora e alcance novos jogadores, fortalecendo sua comunidade com facilidade!
-            </p>
-            <div className="servers__about-button-container">
-              <Button onClick={handleOnClickPublishServer}>PUBLICAR SERVIDOR</Button>
+          <div className="servers__about">
+            <div className="servers__about-left">
+              <header className="servers__about-header">
+                <h1 className="servers__about-title">
+                  Encontre os Melhores Servidores de Minecraft
+                </h1>
+              </header>
+              <p className="servers__about-text">
+                Descubra uma seleção incrível de servidores para todos os estilos de jogo. Conecte-se com comunidades ativas,
+                explore mundos personalizados e participe de aventuras épicas.
+              </p>
+              <p className="servers__about-text">
+                Tem um servidor? Publique-o agora e alcance novos jogadores, fortalecendo sua comunidade com facilidade!
+              </p>
+              <div className="servers__about-button-container">
+                <Button onClick={handleOnClickPublishServer}>PUBLICAR SERVIDOR</Button>
+              </div>
+            </div>
+            <div className="servers__about-right">
+              <div className="servers__about-overlay">
+                <h2>Conecte-se. Explore. Jogue.</h2>
+                <p>Uma experiência Minecraft personalizada, à espera de você.</p>
+              </div>
             </div>
           </div>
-          <div className="servers__about-right">
-            <div className="servers__about-overlay">
-              <h2>Conecte-se. Explore. Jogue.</h2>
-              <p>Uma experiência Minecraft personalizada, à espera de você.</p>
-            </div>
-          </div>
-        </div>
         </section>
 
-
         <section className="servers-list-container">
+          {false ? (
             <ul className="servers-list">
-              {
-                servers.map((server:any) => {
-                  return (
-                    <li key={server.id}  className="servers-list__item">
-                      <ServerCard server={server} showStatus={false}/>
-                    </li>
-                    
-                  )
-                })
-              }
+              {servers.map((server: any) => (
+                <li key={server.id} className="servers-list__item">
+                  <ServerCard server={server} showStatus={false} />
+                </li>
+              ))}
             </ul>
+          ) : (
+            <div className="servers-list__empty">
+              <img className="servers-list__empty-icon" src={diamond}/>
+              <p>Nenhum servidor publicado ainda. Seja o primeiro a publicar e alcance novos jogadores!</p>
+            </div>
+          )}
         </section>
       </section>
     </Fragment>
